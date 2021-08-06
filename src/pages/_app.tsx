9928b49019a3baa58e8520a462a00d97d868b356
tomoki sugihara <sugihara.10969@gmail.com>
2021-08-07 06:57:16 +0900

    Add apollo client provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,20 @@
 import 'tailwindcss/tailwind.css'
 
+import { ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { initializeApollo } from 'src/utils/apolloClient'
 
 const App = (props: AppProps) => {
+  const client = initializeApollo()
   return (
     <>
       <Head>
         <title>nexst</title>
       </Head>
-      <props.Component {...props.pageProps} />
+      <ApolloProvider client={client}>
+        <props.Component {...props.pageProps} />
+      </ApolloProvider>
     </>
   )
 }
